34c7fa09aa4303cb3a5a2c865086c298e362fa7f
PJ Dunlevy <yorko9999@gmail.com>
2014-08-04 14:37:31 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DissRails::Application.config.secret_token = '73b97371f51bad5fb95be96a938ab469e94e9a35b1939a8a048550f754da9c7a6d51dd424effc9ff60c3ef4efd7b0f9642cbda51aec5a7361a44928dcc96f90f'
